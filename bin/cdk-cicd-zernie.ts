9ff223bd48baae14c0da21d0a9771021ea18de1b
Zernie Reyes <zernie@Zernies-MBP.CG-24D>
2024-05-13 15:34:07 +0800

    first commit

---
+++ bin/cdk-cicd-zernie.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkCicdZernieStack } from '../lib/cdk-cicd-zernie-stack';
+
+const app = new cdk.App();
+new CdkCicdZernieStack(app, 'CdkCicdZernieStack', {
+
+});
